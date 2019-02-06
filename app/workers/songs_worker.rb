b9a72345c683c73f525dcfd2bfbd2c2f2a14aa49
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-06 22:20:57 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,7 @@ class SongsWorker
  
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |s|
-      Song.create(name: s[0], artist_name: lead[1], last_name: lead[2])
+      Song.create(name: s[0], artist_name: s[1])
     end
   end
 end
